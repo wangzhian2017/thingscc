4ce168cfc46195688c32030ba1eda5a316e3987e
wangzhian <wangzhian@cmi.chinamobile.com>
2023-12-04 11:39:55 +0800

    .

---
+++ device-mediapipe/ai_code/html2img.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+import time
+from PIL import Image 
+import io
+import base64
+
+def main():
+    response=requests.get('https://www.responsivemiracle.com/smartpack-premium-responsive-html5-template-page-builder/',verify=False)
+   
+    with open(r"C:\\bak\\test.txt","w",encoding="utf-8") as f:
+        f.write(response.text)
+
+    driver=webdriver.Chrome()
+    driver.maximize_window()
+    driver.get('https://www.responsivemiracle.com/smartpack-premium-responsive-html5-template-page-builder/')
+    time.sleep(2)
+    # element=driver.find_element(By.CLASS_NAME,"main")
+    # element.screenshot('C:\\bak\\test.png')
+
+    # 获取网页高度  
+    height = driver.execute_script("return document.body.scrollHeight")  
+    window_size = driver.get_window_size()
+    # 执行滚动操作
+    screenshot = Image.new('RGB', (window_size["width"], height), (255, 255, 255))  
+    scroll_pixels = window_size['height'] # 每次向下滚动的像素数量
+    for i in range(0, height, scroll_pixels):  
+        driver.execute_script("window.scrollTo(0, %d)" % i)  
+        time.sleep(2)  
+        element=driver.find_element(By.ID,"section-content")
+        screenshot_part=Image.open(io.BytesIO(base64.b64decode(element.screenshot_as_base64))).convert("RGB")
+        screenshot.paste(screenshot_part, (0, i)) 
+
+    # 保存截图 
+    screenshot.save("C:\\bak\\test.png")  
+    driver.quit()
+  
+
+
+
+if __name__ == '__main__':
+    main()
